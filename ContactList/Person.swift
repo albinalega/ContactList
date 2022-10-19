9c422c41b312834d57bcad1b64e74a124c0fbfb8
Albina <albina.exe@mail.ru>
2022-10-19 21:58:26 +0200

    WOW i got contact list

--- ContactList/Person.swift
+++ ContactList/Person.swift
@@ -19,26 +19,26 @@ struct Person {
     
     static func getData() -> [Person] {
         var peopleData: [Person] = []
-        for _ in peopleData {
-        if !names.isEmpty {
-            let currentName = names.randomElement() ?? ""
-            let currentSurname = surnames.randomElement() ?? ""
-            let currentPhoneNumber = phoneNumbers.randomElement() ?? ""
-            let currentEmail = emails.randomElement() ?? ""
-            peopleData.append(
-                Person(
-                    name: currentName,
-                    surname: currentSurname,
-                    phoneNumber: currentPhoneNumber,
-                    email: currentEmail
+        for _ in names {
+            if !names.isEmpty {
+                let currentName = names.randomElement() ?? ""
+                let currentSurname = surnames.randomElement() ?? ""
+                let currentPhoneNumber = phoneNumbers.randomElement() ?? ""
+                let currentEmail = emails.randomElement() ?? ""
+                peopleData.append(
+                    Person(
+                        name: currentName,
+                        surname: currentSurname,
+                        phoneNumber: currentPhoneNumber,
+                        email: currentEmail
+                    )
                 )
-            )
-            names = names.filter({ $0 != currentName })
-            surnames = surnames.filter({ $0 != currentSurname })
-            phoneNumbers = phoneNumbers.filter({ $0 != currentPhoneNumber })
-            emails = emails.filter({ $0 != currentEmail })
+                names = names.filter({ $0 != currentName })
+                surnames = surnames.filter({ $0 != currentSurname })
+                phoneNumbers = phoneNumbers.filter({ $0 != currentPhoneNumber })
+                emails = emails.filter({ $0 != currentEmail })
+            }
         }
-    }
         print(peopleData)
         return peopleData
     }
